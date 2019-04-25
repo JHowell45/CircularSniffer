6545e206cacc875e20a8d2ff5c4cc384a969b92a
JHowell45 <jhowell45@googlemail.com>
2019-04-25 22:16:33 +0100

    Created basic CLI file.

--- circularsniffer/cli.py
+++ circularsniffer/cli.py
@@ -1,13 +1,16 @@
-"""
+"""Use this file for creating the CLI for this package.
 
+This file contains the functions used for creating the CLI for the current package to
+allow commands to be executed in a terminal.
 """
 import click
 
-@click.command()
+
+@click.group()
 def main():
-    """"""
-    print('hello world')
+    """CLI for the circular import sniffer application."""
+    pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
